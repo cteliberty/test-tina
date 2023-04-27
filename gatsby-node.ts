d5ebe56f6ce76825a96d82da02162bfdc9b18b0b
Clément Thoret <clement.thoret@eliberty.fr>
2023-04-27 17:15:47 +0200

    fix gatsby node

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,6 +1,6 @@
 const express = require("express");
 
-exports.onCreateDevServer = ({ app }) => {
+exports.onCreateDevServer = ({ app }: any) => {
   app.use("/admin", express.static("public/admin"));
 };
 
